8286b22125c09b5000cda5e610f1d50c2d72d4de
tableonthewall <seydaozdemir07@gmail.com>
2022-08-05 20:57:09 +0300

    fall back methods

--- catalogservice/src/main/java/com/movie/catalogservice/config/CatalogConfiguration.java
+++ catalogservice/src/main/java/com/movie/catalogservice/config/CatalogConfiguration.java
@@ -3,6 +3,7 @@ package com.movie.catalogservice.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,6 +11,8 @@ public class CatalogConfiguration {
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        HttpComponentsClientHttpRequestFactory factory=new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        return new RestTemplate(factory);
     }
  }
--- catalogservice/src/main/java/com/movie/catalogservice/controller/CatalogServiceController.java
+++ catalogservice/src/main/java/com/movie/catalogservice/controller/CatalogServiceController.java
@@ -3,12 +3,14 @@ package com.movie.catalogservice.controller;
 import com.movie.catalogservice.model.CatalogItem;
 import com.movie.catalogservice.model.Movie;
 import com.movie.catalogservice.model.RatingList;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,6 +27,7 @@ public class CatalogServiceController {
 
 
     @GetMapping("/{userId}")
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
         RatingList ratingList=template.getForObject("http://rating-service/ratings/users/"+userId,RatingList.class);
 
@@ -34,5 +37,9 @@ public class CatalogServiceController {
                     Movie movie = template.getForObject("http://movie-info/movies/"+rating.getMovieId(),Movie.class);
                     return new CatalogItem(movie.getName(),movie.getDescription(),rating.getRate());
                 }).collect(Collectors.toList());
+    }
+
+    public List<CatalogItem> getFallbackCatalog(@PathVariable("userId")String userId){
+        return Arrays.asList(new CatalogItem("No movie","",0));
     }
 }
