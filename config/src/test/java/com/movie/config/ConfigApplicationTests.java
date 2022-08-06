6d78dc20d863df1ae98fe013a6586451e2c113a2
tableonthewall <seydaozdemir07@gmail.com>
2022-08-06 16:54:43 +0300

    config service

---
+++ config/src/test/java/com/movie/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
