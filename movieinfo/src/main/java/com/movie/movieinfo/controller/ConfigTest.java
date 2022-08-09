920043673e77a5c2537551b0eac0a4fd289d52c9
tableonthewall <seydaozdemir07@gmail.com>
2022-08-09 23:24:04 +0300

    config service test another service

--- movieinfo/src/main/java/com/movie/movieinfo/controller/ConfigTest.java
+++ movieinfo/src/main/java/com/movie/movieinfo/controller/ConfigTest.java
@@ -1,10 +1,12 @@
 package com.movie.movieinfo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigTest {
     @Value("${app.hello}")
     private String hello;
